d91be1a448d3438739dcf428a9e1638dd57a7a05
Ugo Loobuyck <ugo813@hotmail.fr>
2024-10-11 13:14:45 +0200

    init commit

---
+++ main.py
@@ -0,0 +1,18 @@
+from fastapi import FastAPI, Request
+from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
+
+app = FastAPI()
+
+# Mount static files
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+# Set up Jinja2 templates
+templates = Jinja2Templates(directory="templates")
+
+@app.get("/")
+async def home(request: Request):
+    return templates.TemplateResponse("index.html", {
+        "request": request,
+        "title": "Home",
+    })
